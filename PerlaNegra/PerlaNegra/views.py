d45ef4b18ab082c8f4586236116af4cd1aa1473b
RickyRicardo19 <ricardoalfcardozo@gmail.com>
2023-01-22 18:59:32 -0300

    1er commit

---
+++ PerlaNegra/PerlaNegra/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+def inicio(request):
+    templates_name = 'index.html'
+    contexto = {
+     }
+    return render(request, templates_name, contexto)
---
+++ PerlaNegra/producto/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+class Producto(models.Model):
+    nombre = models.CharField(max_length=255)
+    precio = models.DecimalField(max_digits=6, decimal_places=2)
+    descripcion = models.TextField()
